2691dc95473969bb9111947df060328b092e1007
daes79 <daesonjakob@gmail.com>
2018-01-16 22:10:34 -0500

    Create power_boot_restore.py

---
+++ power_boot_restore.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import paramiko
+import sys
+import time
+
+hostname = "host-ip" #hostip
+idrac = "idrac-ip" #idracip
+idracUN = "youridracUN"
+idracPW = "youridracPW"
+waittime = "WaitTimeInSeconds"
+
+#time.sleep(waittime) #Uncomment if you'd like a dealy to ensure power is stable
+response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1") #ping host
+#check response
+if response == 0: #host is up
+  print('Host online')
+  sys.exit()
+else: #if host can't be pinged
+  response = os.system("ping -c 1 -w2 " + idrac + " > /dev/null 2>&1") #ping idrac
+  if response == 0: #idrac is up
+    ssh = paramiko.SSHClient() #ssh into idrac
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect("idrac", port=22, username="idracUN", password="idracPW", look_for_keys=False)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('racadm serveraction powerup')
+    print('Starting host') 
+  else: #if idrac is down
+     print('iDrac down!')
+     sys.exit() 
